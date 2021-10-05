10e722faf10fe763bed51b928f77636f30e4cb1e
artem.zagoveliev <mexell96@gmail.com>
2021-10-05 12:50:14 +0300

    first commit

---
+++ src/components/PostForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent>
+    <h4>Create post</h4>
+    <my-input v-model="post.title" type="text" placeholder="Name" />
+    <my-input v-model="post.body" type="text" placeholder="Description" />
+    <my-button
+      style="align-self: flex-end;  margin-top: 15px;"
+      @click="createPost"
+      >Create</my-button
+    >
+  </form>
+</template>
+
+<script>
+import MyButton from "./UI/MyButton.vue";
+import MyInput from "./UI/MyInput.vue";
+export default {
+  components: { MyButton, MyInput },
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post);
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
